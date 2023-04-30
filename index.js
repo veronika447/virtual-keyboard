61ce88a8e14b9747a158f09805b1f202a1d2b24e
veronika447 <starenkaya.veronika@gmail.com>
2023-04-30 15:20:21 +0300

    feat: add functionality key Shift

--- index.js
+++ index.js
@@ -13,56 +13,113 @@ document.body.append(textarea);
 let list = document.createElement('ul');
 list.className = "list";
 
-let arrItem = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '&#92;', 'Del',
-    'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '&#39;', 'Enter',
-    'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '⏶', 'Shift',
-    'Ctrl', 'Win', 'Alt', ' ', 'Alt', '⏴', '⏷', '⏵', 'Ctrl']
-for (let i = 0; i < arrItem.length; i++) {
-    let listItem = document.createElement('li');
-    listItem.innerHTML = arrItem[i];
-    listItem.className = "list-item";
-    if ((i >= 0 && i < 13) || (i >= 15 && i < 28) || (i >= 30 && i < 41) || (i >= 43 && i < 53) || i === 58) {
-        listItem.classList.add('symbol');
-        // if ((i >= 0 && i <= 11) || (i >= 25 && i <= 27) || i === 39 || i === 40 || (i >= 50 && i <= 52)) {
-        // }
-        if (arrItem[i] == ' ') {
-            listItem.classList.add('space')
-        }
-        // else { listItem.classList.add('letter'); }
-    }
-    else {
-        listItem.classList.add('func');
+let listItemEn = [['`', '~'], ['1', '!'], ['2', '@'], ['3', '#'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('], ['0', ')'], ['-', '_'], ['=', '+'], 'Backspace',
+    'Tab', ['q', 'Q'], ['w', 'W'], ['e', 'E'], ['r', 'R'], ['t', 'T'], ['y', 'Y'], ['u', 'U'], ['i', 'I'], ['o', 'O'], ['p', 'P'], ['[', '{'], [']', '}'], ['&#92;', '|'], 'Del',
+    'Caps Lock', ['a', 'A'], ['s', 'S'], ['d', 'D'], ['f', 'F'], ['g', 'G'], ['h', 'H'], ['j', 'J'], ['k', 'K'], ['l', 'L'], [';', ':'], ['&#39;', '"'], 'Enter',
+    'Shift', ['z', 'Z'], ['x', 'X'], ['c', 'C'], ['v', 'V'], ['b', 'B'], ['n', 'N'], ['m', 'M'], [',', '<'], ['.', '>'], ['/', '?'], '⏶', 'Shift',
+    'Ctrl', 'Win', 'Alt', [' '], 'Alt', '⏴', '⏷', '⏵', 'Ctrl'];
 
-        if (arrItem[i] == 'Shift') {
-            listItem.classList.add('shift')
+// let listItemEnUp = {
+//     0: '~', 1: '!', 2: '@', 3: '#', 4: '$', 5: '%', 6: '^', 7: '&', 8: '*', 9: '(', 10: ')', 11: '_', 12: '+',
+//     23: '{', 24: '}', 25: '|', 35: ':', 36: '""', 44: '<', 45: '>', 46: '?'
+// }
 
-        }
-        if (arrItem[i] == 'Enter') {
-            listItem.classList.add('enter')
 
-        }
-        if (arrItem[i] == 'Backspace') {
-            listItem.classList.add('backspace')
-
-        }
-        if (arrItem[i] == 'Caps Lock') {
-            listItem.classList.add('caps')
+function createKeyboard(arr) {
+    arr.forEach(el => {
+        let listItem = document.createElement('li');
+        listItem.className = "list-item";
+        if (Array.isArray(el)) {
+            listItem.classList.add('symbol');
+            if (el.length === 1) {
+                listItem.classList.add('space')
+            }
+            el.forEach((item, index) => {
+                let listItemSpan = document.createElement('span');
+                listItemSpan.innerHTML = item;
+                listItemSpan.className = "span";
+                if (index === 1)
+                    listItemSpan.classList.add('off');
+                if (index === 0)
+                    listItemSpan.classList.add('on')
+                listItem.appendChild(listItemSpan);
+            });
 
         }
-    }
-    list.appendChild(listItem);
+        else {
+            listItem.innerHTML = el;
+            listItem.classList.add('func');
+            if (el == 'Shift') {
+                listItem.classList.add('shift')
+
+            }
+            if (el == 'Enter') {
+                listItem.classList.add('enter')
+
+            }
+            if (el == 'Backspace') {
+                listItem.classList.add('backspace')
+
+            }
+            if (el == 'Caps Lock') {
+                listItem.classList.add('caps')
+
+            }
+        }
+        list.appendChild(listItem);
+    })
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     let listItem = document.createElement('li');
+    //     listItem.innerHTML = arr[i];
+    //     listItem.className = "list-item";
+    //     if ((i >= 0 && i < 13) || (i >= 15 && i < 28) || (i >= 30 && i < 41) || (i >= 43 && i < 53) || i === 58) {
+    //         listItem.classList.add('symbol');
+    //         if (arr[i] == ' ') {
+    //             listItem.classList.add('space')
+    //         }
+    //     }
+    //     else {
+    //         listItem.classList.add('func');
+
+    //         if (arr[i] == 'Shift') {
+    //             listItem.classList.add('shift')
+
+    //         }
+    //         if (arr[i] == 'Enter') {
+    //             listItem.classList.add('enter')
+
+    //         }
+    //         if (arr[i] == 'Backspace') {
+    //             listItem.classList.add('backspace')
+
+    //         }
+    //         if (arr[i] == 'Caps Lock') {
+    //             listItem.classList.add('caps')
+
+    //         }
+    //     }
+    //     list.appendChild(listItem);
+    // }
+
+    return document.body.append(list);
 }
 
-
-document.body.append(list);
+createKeyboard(listItemEn)
 
 const symbols = document.querySelectorAll('.symbol');
 const listItems = document.querySelectorAll('.list-item');
+const symbolsSpan = document.querySelectorAll('.span');
+const symbolSpanOn = document.querySelectorAll('.on')
 
 window.addEventListener("keydown", (e) => {
+    symbolSpanOn.forEach(item => {
+        if (e.key == item.textContent || e.key == item.textContent.toLocaleUpperCase()) {
+            item.parentElement.classList.add('active')
+        }
+    })
     listItems.forEach((el, i) => {
-        if ((e.key == el.textContent || e.key == el.textContent.toUpperCase()) && el.textContent !== 'Alt' && el.textContent !== 'Shift')
+        if ((e.key == el.textContent) && el.textContent !== 'Alt' && el.textContent !== 'Shift')
             el.classList.add('active');
         if (e.code == 'ControlLeft' && el.textContent == 'Ctrl' && i === 55) {
             el.classList.add('active')
@@ -95,10 +152,12 @@ window.addEventListener("keydown", (e) => {
             el.classList.toggle('active')
         }
         if (e.code == 'ShiftLeft' && i === 42) {
-            el.classList.add('active')
+            el.classList.add('active');
+            shiftToDo();
         }
         if (e.code == 'ShiftRight' && i === 54) {
-            el.classList.add('active')
+            el.classList.add('active');
+            shiftToDo();
         }
         if (e.code == 'AltLeft' && i === 57) {
             el.classList.add('active')
@@ -110,8 +169,15 @@ window.addEventListener("keydown", (e) => {
 });
 
 window.addEventListener("keyup", (e) => {
+    symbolSpanOn.forEach(item => {
+        if (e.key == item.textContent || e.key == item.textContent.toLocaleUpperCase()) {
+            item.parentElement.classList.remove('active')
+        }
+    })
     listItems.forEach((el, i) => {
-        if (e.key == el.textContent || e.key == el.textContent.toUpperCase()) { el.classList.remove('active') }
+        if (e.key == el.textContent) {
+            el.classList.remove('active')
+        }
         if (e.key == 'Control' && el.textContent == 'Ctrl') {
             el.classList.remove('active')
         }
@@ -136,14 +202,46 @@ window.addEventListener("keyup", (e) => {
         if (e.code == 'ArrowRight' && i === 62) {
             el.classList.remove('active')
         }
-        // if (e.code == 'CapsLock' && el.textContent == 'Caps Lock') {
-        //     el.classList.remove('active')
-        // }
+        if (e.code == 'ShiftLeft' && i === 42) {
+            shiftToDo();
+        }
+        if (e.code == 'ShiftRight' && i === 54) {
+            shiftToDo();
+        }
+
     })
 })
 
 symbols.forEach(el => {
-    el.addEventListener("click", (e) => {
-        textarea.value += el.textContent
+    el.addEventListener('click', (e) => {
+        for (let child of el.children) {
+            if (child.classList.contains('on'))
+                textarea.value += child.textContent
+        }
     })
 })
+
+const shift = document.querySelectorAll('.shift')
+
+function shiftToDo() {
+    symbolsSpan.forEach(el => {
+        if (el.classList.contains('on') && el.textContent !== ' ') {
+            el.classList.remove('on');
+            el.classList.add('off')
+        }
+        else if (el.classList.contains('off')) {
+            el.classList.remove('off');
+            el.classList.add('on')
+        }
+
+    })
+}
+
+shift.forEach(el => {
+    el.addEventListener('mousedown', shiftToDo)
+})
+shift.forEach(el => {
+    el.addEventListener('mouseup', shiftToDo)
+})
+
+
